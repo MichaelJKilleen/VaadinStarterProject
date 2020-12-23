4d5a95e36a724d436e5566f38fd8830a355e0756
Michael Killeen <michaeljkilleen@outlook.com>
2020-12-23 15:13:58 -0500

    Initial

---
+++ src/main/java/com/yahoo/michael/ui/components/detailsdrawer/DetailsDrawerFooter.java
@@ -0,0 +1,40 @@
+package com.yahoo.michael.ui.components.detailsdrawer;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.shared.Registration;
+import com.yahoo.michael.ui.components.FlexBoxLayout;
+import com.yahoo.michael.ui.layout.size.Horizontal;
+import com.yahoo.michael.ui.layout.size.Right;
+import com.yahoo.michael.ui.layout.size.Vertical;
+import com.yahoo.michael.ui.util.LumoStyles;
+import com.yahoo.michael.ui.util.UIUtils;
+
+public class DetailsDrawerFooter extends FlexBoxLayout {
+
+	private Button save;
+	private Button cancel;
+
+	public DetailsDrawerFooter() {
+		setBackgroundColor(LumoStyles.Color.Contrast._5);
+		setPadding(Horizontal.RESPONSIVE_L, Vertical.S);
+		setSpacing(Right.S);
+		setWidthFull();
+
+		save = UIUtils.createPrimaryButton("Save");
+		cancel = UIUtils.createTertiaryButton("Cancel");
+		add(save, cancel);
+	}
+
+	public Registration addSaveListener(
+			ComponentEventListener<ClickEvent<Button>> listener) {
+		return save.addClickListener(listener);
+	}
+
+	public Registration addCancelListener(
+			ComponentEventListener<ClickEvent<Button>> listener) {
+		return cancel.addClickListener(listener);
+	}
+
+}
